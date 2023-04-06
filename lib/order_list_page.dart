ab9791780fc35d300c4c2ff9ab26dc2d9f5f9959
Anshu-Jaiswal <anshu.jaiswal1903@gmail.com>
2023-04-06 11:24:59 +0530

    Added Exit Confirmation Logic on OrderListPage

--- lib/order_list_page.dart
+++ lib/order_list_page.dart
@@ -7,49 +7,72 @@ import 'order_confirm_page.dart';
 
 class OrderListPage extends StatelessWidget {
   const OrderListPage({Key? key}) : super(key: key);
+  Future<bool> _onWillPop(BuildContext context) async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to exit the App'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(false),
+                child: Text('No'),
+              ),
+              TextButton(
+                onPressed: () => Navigator.of(context).pop(true),
+                child: Text('Yes'),
+              ),
+            ],
+          ),
+        )) ??
+        false;
+  }
 
   @override
   Widget build(BuildContext context) {
     final list = context.read<ItemProvider>().list;
-    return Scaffold(
-      backgroundColor: Colors.amber,
-      appBar: AppBar(),
-      body: Stack(
-        children: [
-          ListView.builder(
-            itemCount: list.length,
-            itemBuilder: (context, index) {
-              var item = list[index];
-              return ListTile(
-                title: Text(item.name),
-                trailing: _UpdateOrderWidget(item),
-              );
-            },
-          ),
-          Positioned.fill(
-            child: Align(
-              alignment: Alignment.bottomLeft,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.only(
-                    bottomRight: Radius.circular(100),
-                    topRight: Radius.circular(100),
+    return WillPopScope(
+      onWillPop: () => Future.value(_onWillPop(context)),
+      child: Scaffold(
+        backgroundColor: Colors.amber,
+        appBar: AppBar(),
+        body: Stack(
+          children: [
+            ListView.builder(
+              itemCount: list.length,
+              itemBuilder: (context, index) {
+                var item = list[index];
+                return ListTile(
+                  title: Text(item.name),
+                  trailing: _UpdateOrderWidget(item),
+                );
+              },
+            ),
+            Positioned.fill(
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(100),
+                      topRight: Radius.circular(100),
+                    ),
                   ),
+                  padding: const EdgeInsets.fromLTRB(16, 16, 24, 16),
+                  child: Text(
+                      "Total: ${context.select((ItemProvider p) => p.total)}"),
                 ),
-                padding: const EdgeInsets.fromLTRB(16, 16, 24, 16),
-                child: Text(
-                    "Total: ${context.select((ItemProvider p) => p.total)}"),
               ),
             ),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(context,
-              MaterialPageRoute(builder: (context) => OrderConfirmPage()));
-        },
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => OrderConfirmPage()));
+          },
+        ),
       ),
     );
   }
